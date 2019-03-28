209bade8c9dc4be6bcc5f2a06089bbf135d2c956
Matt Chapman <matt@mttchpmn.com>
2019-03-28 13:12:38 +1300

    Add http test

---
+++ http_get_test/http_get_test.ino
@@ -0,0 +1,60 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+ 
+/* Set these to your desired credentials. */
+const char *ssid = "ChapChapChow";  //ENTER YOUR WIFI SETTINGS
+const char *password = "gottagetdownonfriday";
+ 
+//Web/Server address to read/write from 
+const char *host = "192.168.1.7";   
+//=======================================================================
+//                    Power on setup
+//=======================================================================
+ 
+void setup() {
+  delay(1000);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_OFF);        //Prevents reconnection issue (taking too long to connect)
+  delay(1000);
+  WiFi.mode(WIFI_STA);        //This line hides the viewing of ESP as wifi hotspot
+  
+  WiFi.begin(ssid, password);     //Connect to your WiFi router
+  Serial.println("");
+ 
+  Serial.print("Connecting");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+}
+ 
+//=======================================================================
+//                    Main Program Loop
+//=======================================================================
+void loop() {
+  HTTPClient http;    //Declare object of class HTTPClient
+ 
+  String Address = "http://192.168.1.7:5000/node?id=AAA111";
+  
+  http.begin(Address);     //Specify request destination
+  
+  int httpCode = http.GET();            //Send the request
+  String payload = http.getString();    //Get the response payload
+ 
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.println(payload);    //Print request response payload
+ 
+  http.end();  //Close connection
+  
+  delay(5000);  //GET Data at every 5 seconds
+}
